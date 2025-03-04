c3d26e7e6e084463d87c78972a7f30c84cfd19c6
Reynald Ortiga <keanortega09@gmail.com>
2025-03-04 09:44:23 +0800

    feat: add delete method to ITodoRepository interface

--- src/domain/interfaces/ITodoRepository.ts
+++ src/domain/interfaces/ITodoRepository.ts
@@ -1,9 +1,13 @@
-import { Todo } from "../entities/Todo";
+import { Todo } from '../entities/Todo';
 
 export interface ITodoRepository {
       getAll(): Promise<Todo[]>;
+
       getById(id: string): Promise<Todo | null>;
+
       create(todo: Todo): Promise<Todo>;
+
       update(todo: Todo): Promise<Todo>;
+
       delete(id: string): Promise<void>;
 }
