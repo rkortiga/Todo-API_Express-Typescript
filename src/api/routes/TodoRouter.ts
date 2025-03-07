52d3d49cfbac12e48de113db091c0ef919a57553
Reynald Ortiga <keanortega09@gmail.com>
2025-03-08 04:22:17 +0800

    Add validators in the POST and PUT routes

--- src/api/routes/TodoRouter.ts
+++ src/api/routes/TodoRouter.ts
@@ -2,6 +2,8 @@ import { Request, Response, Router } from 'express';
 import { TodoController } from '../controllers/TodoController';
 import { TodoService } from '../../application/services/TodoService';
 import { TodoRepository } from '../../infrastructure/repositories/TodoRepository';
+import { Validator } from '../middleware/Validator';
+import { UpdateTodoDto } from '../../domain/dtos/UpdateTodoDto';
 
 const router = Router();
 
@@ -10,9 +12,13 @@ const todoService = new TodoService(todoRepository);
 const todoController = new TodoController(todoService);
 
 router.get('/', (req: Request, res: Response) => todoController.getAll(req, res));
-router.get('/:id', (req: Request, res: Response) => todoController.getById(req, res));
-router.post('/', (req: Request, res: Response) => todoController.create(req, res));
-router.put('/:id', (req: Request, res: Response) => todoController.update(req, res));
-router.delete('/:id', (req: Request, res: Response) => todoController.delete(req, res));
+router.get('/:id', (req: Request<{ id: string }>, res: Response) => todoController.getById(req, res));
+router.post('/', Validator.validateCreateTodo, (req: Request, res: Response) => todoController.create(req, res));
+router.put(
+      '/:id',
+      Validator.validateUpdateTodo,
+      (req: Request<{ id: string }, any, UpdateTodoDto>, res: Response) => todoController.update(req, res)
+);
+router.delete('/:id', (req: Request<{ id: string }>, res: Response) => todoController.delete(req, res));
 
 export default router;
