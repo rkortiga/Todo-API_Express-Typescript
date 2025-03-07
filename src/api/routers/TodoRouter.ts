5122660a474e0fc9ea2dc237ce8c0953f3f2d797
Reynald Ortiga <keanortega09@gmail.com>
2025-03-07 17:12:11 +0800

    Add type annotations to request and response in router

--- src/api/routers/TodoRouter.ts
+++ src/api/routers/TodoRouter.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import { Request, Response } from 'express';
 import { TodoController } from '../controllers/TodoController';
 import { TodoService } from '../../application/services/TodoService';
 import { SqlServerTodoRepository } from '../../infrastructure/repositories/SqlServerTodoRepository';
@@ -11,10 +12,10 @@ const todoService = new TodoService(todoRepository);
 const todoController = new TodoController(todoService);
 
 // Map routes to controller methods.
-router.get('/', (req, res) => todoController.getAll(req, res));
-router.get('/:id', (req, res) => todoController.getById(req, res));
-router.post('/', (req, res) => todoController.create(req, res));
-router.put('/:id', (req, res) => todoController.update(req, res));
-router.delete('/:id', (req, res) => todoController.delete(req, res));
+router.get('/', (req: Request, res: Response) => todoController.getAll(req, res));
+router.get('/:id', (req: Request, res: Response) => todoController.getById(req, res));
+router.post('/', (req: Request, res: Response) => todoController.create(req, res));
+router.put('/:id', (req: Request, res: Response) => todoController.update(req, res));
+router.delete('/:id', (req: Request, res: Response) => todoController.delete(req, res));
 
 export default router;
