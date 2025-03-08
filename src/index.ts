009f7910bd9bec6661ed2846b22e30861ea54744
Reynald Ortiga <keanortega09@gmail.com>
2025-03-08 18:59:40 +0800

    Move app data source into its own file

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,10 @@
 import 'reflect-metadata';
 import express from 'express';
 import dotenv from 'dotenv';
-import { DataSource } from 'typeorm';
 import swaggerUi from 'swagger-ui-express';
 import swaggerSpec from './swagger';
 import todoRouter from './api/routes/TodoRouter';
-import { DatabaseConfig } from './infrastructure/configurations/DatabaseConfig';
+import { AppDataSource } from './infrastructure/configurations/app-data-source';
 
 dotenv.config();
 
@@ -16,25 +15,11 @@ app.use(express.json());
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 app.use('/todos', todoRouter);
 
-const AppDataSource = new DataSource({
-      type: 'mssql',
-      host: DatabaseConfig.server,
-      port: DatabaseConfig.port,
-      username: DatabaseConfig.user,
-      password: DatabaseConfig.password,
-      database: DatabaseConfig.database,
-      entities: ['src/infrastructure/entities/**/*.ts'],
-      synchronize: true,
-      options: DatabaseConfig.options,
-});
-
 AppDataSource.initialize()
       .then(() => {
             app.listen(PORT, () => {
                   console.log(`Server is running on port ${PORT}`);
-                  console.log(
-                        `Swagger UI available at http://localhost:${PORT}/api-docs`
-                  );
+                  console.log(`Swagger UI available at http://localhost:${PORT}/api-docs`);
             });
       })
       .catch((error) => console.error('Connection error: ', error));
---
+++ src/infrastructure/configurations/app-data-source.ts
@@ -0,0 +1,16 @@
+import { DatabaseConfig } from './database-config';
+import { DataSource } from 'typeorm';
+import 'reflect-metadata';
+
+export const AppDataSource = new DataSource({
+      type: 'mssql',
+      host: DatabaseConfig.server,
+      port: DatabaseConfig.port,
+      username: DatabaseConfig.user,
+      password: DatabaseConfig.password,
+      database: DatabaseConfig.database,
+      entities: ['src/infrastructure/entities/**/*.ts'],
+      synchronize: true,
+      migrations: ['src/infrastructure/migrations/**/*.ts'],
+      options: DatabaseConfig.options
+});
