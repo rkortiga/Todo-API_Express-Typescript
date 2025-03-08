03beea634dcdb9108a7e40714c26059960b90ba2
Reynald Ortiga <keanortega09@gmail.com>
2025-03-08 19:01:23 +0800

    Modify docker-entrypoint.sh to include migration run and sql server check prior to api start up

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,8 +1,20 @@
 #!/bin/sh
 set -e
 
-echo "Initializing database..."
+echo "Waiting for SQL Server to be ready..."
+while ! nc -z db 1433; do
+  sleep 2
+  echo "Waiting for SQL Server..."
+done
+
+echo "SQL Server is ready. Initializing database..."
 npm run init-db
+
+#echo "Generating migration..."
+#npm run migration:generate -- src/infrastructure/migrations/Migration$(date +%s)
+
+echo "Running migrations..."
+npm run migration:run
 
 echo "Starting API..."
 npm run dev
